b8a8b582b72bd048ea363df5fe35b9b5de48073c
zhorvat8 <zhorvat8@DHUB4552.pc.hu.t-internal.com>
2018-01-08 11:08:06 +0100

    1st

---
+++ TestProj/src/sdkcheck.java
@@ -0,0 +1,19 @@
+/**
+ * GitHub version
+ */
+
+/**
+ * @author zhorvat8
+ *
+ */
+public class sdkcheck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("HELLO Test");
+
+	}
+
+}
