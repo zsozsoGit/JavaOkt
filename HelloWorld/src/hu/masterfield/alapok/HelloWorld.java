9d6b86471abf6a15d2b1a0039d1e87fb54440313
zhorvat8 <zhorvat8@DHUB4552.pc.hu.t-internal.com>
2018-01-08 12:06:41 +0100

    First fa.

---
+++ HelloWorld/src/hu/masterfield/alapok/HelloWorld.java
@@ -0,0 +1,11 @@
+package hu.masterfield.alapok;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HW1");
+
+	}
+
+}
