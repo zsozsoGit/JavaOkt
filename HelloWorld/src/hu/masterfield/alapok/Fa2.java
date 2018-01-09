a3fca8ecd16abe0e05deb4b87fc6435960f0c986
zhorvat8 <zhorvat8@DHUB4552.pc.hu.t-internal.com>
2018-01-09 11:23:10 +0100

    Fa2

--- HelloWorld/src/hu/masterfield/alapok/Fa2.java
+++ HelloWorld/src/hu/masterfield/alapok/Fa2.java
@@ -7,10 +7,21 @@ public class Fa2 {
 	public static void main(String[] args) {
 
 		Random randa = new Random();
-		System.out.println(randa.nextInt(10));
+		//System.out.println(randa.nextInt(10));
 		int meret = randa.nextInt(11)+20;
 		int [] x = new int [meret];
-		//(randa.nextInt());
+		for (int i = 0; i < x.length; i++) {
+			x[i] = randa.nextInt(999)+1001;
+		}
+		System.out.println(x.length);
+		System.out.println("---------------");
+		float sum = 0;
+		for (int i : x) {
+			System.out.print(" "+i);
+			sum +=i;
+		}
+		System.out.println("\n---------------");
+		System.out.println("sum: "+ sum + " atlag: "+(sum/(x.length)));
 	}
 
 }
